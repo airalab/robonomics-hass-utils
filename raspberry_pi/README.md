1fadb7057882f05b27ee530121ffd5d10a8b62d2
LoSk-p <alki.spb@mail.ru>
2022-10-01 18:13:35 +0300

    add installation script for preinstalled Home Assistant Core

--- raspberry_pi/README.md
+++ raspberry_pi/README.md
@@ -4,7 +4,8 @@ This repository contains scripts for installation process on RPI.
 
 Next two scripts for public usage:
 
- - 'install.sh' - bash script for main installation process.
- - 'mqtt-install.sh' - bash script for mqtt broker installation.
+ - `install.sh` - bash script for main installation process.
+ - `mqtt-install.sh` - bash script for mqtt broker installation.
+ - `install_integration.sh` - bash script for Robonomics integration installation on preinstalled Home Assistant Core.
 
 Other two scripts for technical usage. they need to create prebuild and ready-yo-use RPI image.
---
+++ raspberry_pi/install_integration.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+cd /home/$USER
+
+arc=$(lscpu | grep Architecture: | awk '{print $2}')
+echo $arc
+if [ $arc = "x86_64" ]; then 
+    wget https://dist.ipfs.io/go-ipfs/v0.14.0/go-ipfs_v0.14.0_linux-amd64.tar.gz
+else
+    wget https://dist.ipfs.io/go-ipfs/v0.14.0/go-ipfs_v0.14.0_linux-arm64.tar.gz
+fi
+
+tar -xvzf go-ipfs_v0.14.0_linux-arm64.tar.gz
+rm go-ipfs_v0.14.0_linux-arm64.tar.gz
+cd go-ipfs
+sudo bash install.sh
+ipfs init -p local-discovery
+ipfs bootstrap add /dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8
+ipfs bootstrap add /dns4/2.pubsub.aira.life/tcp/443/wss/ipfs/QmPTFt7GJ2MfDuVYwJJTULr6EnsQtGVp8ahYn9NSyoxmd9
+ipfs bootstrap add /dns4/3.pubsub.aira.life/tcp/443/wss/ipfs/QmWZSKTEQQ985mnNzMqhGCrwQ1aTA6sxVsorsycQz9cQrw
+
+echo "[Unit]
+Description=IPFS Daemon Service
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/ipfs daemon
+User=$USER
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/ipfs-daemon.service
+
+sudo systemctl enable ipfs-daemon.service
+sudo systemctl start ipfs-daemon.service
+
+echo "DONE WITH IPFS"
+
+sudo apt-get install -y subversion
+
+sudo -u homeassistant -H -s bash -c "cd /home/homeassistant/.homeassistant &&
+                                     mkdir custom_components &&
+                                     cd custom_components &&
+                                     svn checkout https://github.com/airalab/homeassistant-robonomics-integration/trunk/custom_components/robonomics"
