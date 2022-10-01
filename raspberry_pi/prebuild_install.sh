724e9ce9966cc5b6356ca2bb7edbdff06a3b41af
nakata5321 <makar5321@gmail.com>
2022-10-01 11:16:38 +0300

    add url to file

--- raspberry_pi/prebuild_install.sh
+++ raspberry_pi/prebuild_install.sh
@@ -31,9 +31,14 @@ tar -xvzf go-ipfs_v0.14.0_linux-arm64.tar.gz
 rm go-ipfs_v0.14.0_linux-arm64.tar.gz
 cd go-ipfs
 sudo bash install.sh
-ipfs init
+ipfs init -p local-discovery
+ipfs bootstrap add /dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8
+ipfs bootstrap add /dns4/2.pubsub.aira.life/tcp/443/wss/ipfs/QmPTFt7GJ2MfDuVYwJJTULr6EnsQtGVp8ahYn9NSyoxmd9
+ipfs bootstrap add /dns4/3.pubsub.aira.life/tcp/443/wss/ipfs/QmWZSKTEQQ985mnNzMqhGCrwQ1aTA6sxVsorsycQz9cQrw
+
 
 cd /home/$USER
+curl -O https://raw.githubusercontent.com/LoSk-p/robonomics-hass-utils/main/raspberry_pi/ipfs_check.sh
 sudo chmod a+x ipfs_check.sh
 sudo mv ipfs_check.sh /usr/local/bin/
 
@@ -111,7 +116,7 @@ sudo systemctl restart home-assistant@homeassistant.service
 
 echo "Clear garbage"
 cd /home/$USER
-rm install.sh
+rm prebuild_install.sh
 rm -r go-ipfs/
 rm .ipfs/check
 
