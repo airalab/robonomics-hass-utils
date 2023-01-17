949e6844bda72cd5c8b9d3c0bd363c7a0522b892
nakata5321 <makar5321@gmail.com>
2023-01-17 13:02:49 +0700

    add spb bootstrap and turn on auto GC collect

--- raspberry_pi/install_ipfs_arc_dependent.sh
+++ raspberry_pi/install_ipfs_arc_dependent.sh
@@ -24,13 +24,14 @@ ipfs init -p local-discovery
 ipfs bootstrap add /dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8
 ipfs bootstrap add /dns4/2.pubsub.aira.life/tcp/443/wss/ipfs/QmPTFt7GJ2MfDuVYwJJTULr6EnsQtGVp8ahYn9NSyoxmd9
 ipfs bootstrap add /dns4/3.pubsub.aira.life/tcp/443/wss/ipfs/QmWZSKTEQQ985mnNzMqhGCrwQ1aTA6sxVsorsycQz9cQrw
+ipfs bootstrap add /dns4/ipfs-gateway.multi-agent.io/tcp/4001/ipfs/12D3KooWAuRhU7tnQ3cVQxvvdEHZ4pnfwipMtDL3oxZuzk9wP649
 
 echo "[Unit]
 Description=IPFS Daemon Service
 
 [Service]
 Type=simple
-ExecStart=/usr/local/bin/ipfs daemon
+ExecStart=/usr/local/bin/ipfs daemon --enable-gc
 User=$USER
 
 [Install]
