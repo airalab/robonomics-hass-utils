0d40ed34a104df2693584bdde9c077a50a087c31
nakata5321 <makar5321@gmail.com>
2023-01-23 21:07:06 +0700

    download input file

--- raspberry_pi/first_start.sh
+++ raspberry_pi/first_start.sh
@@ -23,11 +23,11 @@ else
     echo "initializing yggdrasil"
 
     yggdrasil -genconf -json > ./ygg.conf
+    curl -O https://raw.githubusercontent.com/airalab/robonomics-hass-utils/main/raspberry_pi/input.json
+
     jq '.Peers = input' ygg.conf input.json > yggdrasil.conf
-    rm ygg.conf
+
     chmod 664 yggdrasil.conf
     sudo mv yggdrasil.conf /etc/
-    sudo systemctl enable yggdrasil
-    sudo systemctl start yggdrasil
     echo "initialized yggdrasil"
 fi
