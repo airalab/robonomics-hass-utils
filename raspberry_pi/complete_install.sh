38e674ae81bc831e97e444466d6bb427732f6714
nakata5321 <makar5321@gmail.com>
2023-09-08 15:49:59 +0300

    update scripts

--- raspberry_pi/complete_install.sh
+++ raspberry_pi/complete_install.sh
@@ -5,11 +5,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y python3 python3-dev python3-venv python3-pip bluez libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata libcurl4-openssl-dev python3-serial curl unzip
 
-sudo useradd -rm homeassistant
-sudo mkdir /srv/homeassistant
-sudo chown homeassistant:homeassistant /srv/homeassistant
-sudo usermod -a -G tty homeassistant
-sudo usermod -a -G dialout homeassistant
+sudo useradd -rm homeassistant -d /srv/homeassistant -G dialout,tty
 
 cd /srv/homeassistant
 sudo -u homeassistant -H -s python3 -m venv .
@@ -20,7 +16,7 @@ sudo -u homeassistant -H -s bash -c "source bin/activate &&
 				     pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
 
 sudo -u homeassistant -H -s bash -c "source bin/activate &&
-				     pip3 install homeassistant==2023.5.3 psutil-home-assistant~=0.0 &&
+				     pip3 install homeassistant==2023.5.4 psutil-home-assistant~=0.0 &&
         			     timeout 60s hass"
 			     
 
@@ -32,19 +28,44 @@ sudo apt-get install -y jq dirmngr
 cd
 wget https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v0.4.7/yggdrasil-0.4.7-arm64.deb
 sudo dpkg -i yggdrasil-0.4.7-arm64.deb
+rm yggdrasil-0.4.7-arm64.deb
 rm -f /etc/yggdrasil.conf
 
-cd /home/${USER}
+cd
 curl -O https://raw.githubusercontent.com/airalab/robonomics-hass-utils/main/raspberry_pi/input.json
+
+yggdrasil -genconf -json > ./ygg.conf
+
+sudo jq '.Peers = input' ygg.conf input.json > /etc/yggdrasil.conf
+
+sudo chmod 664 /etc/yggdrasil.conf
+rm ygg.conf
+rm input.json
 
 echo "done with yggdrasil"
 echo "install ipfs"
 
-wget https://raw.githubusercontent.com/airalab/robonomics-hass-utils/main/raspberry_pi/install_ipfs_arc_dependent.sh
-bash install_ipfs_arc_dependent.sh
+sudo useradd -rm ipfsdaemon -d /var/lib/ipfsdaemon
+
+cd /var/lib/ipfsdaemon/
+sudo su ipfsdaemon -c "wget https://raw.githubusercontent.com/airalab/robonomics-hass-utils/main/raspberry_pi/install_ipfs_arc_dependent.sh"
+sudo su ipfsdaemon -c  bash install_ipfs_arc_dependent.sh
 rm install_ipfs_arc_dependent.sh
 
 echo "done with ipfs"
+
+echo "Install MQTT"
+
+sudo apt-get install -y mosquitto mosquitto-clients
+
+echo "listener 1883
+allow_anonymous false
+password_file /etc/mosquitto/passwd" | sudo tee /etc/mosquitto/conf.d/local.conf
+
+chmod o=rwx /etc/mosquitto/
+
+sudo sed -i 's/Restart=on-failure/Restart=always\nRestartSec=15/' /lib/systemd/system/mosquitto.service
+
 echo "install z2m"
 
 sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
@@ -54,11 +75,11 @@ node --version
 npm --version
 
 sudo mkdir /opt/zigbee2mqtt
-sudo chown -R ${USER}: /opt/zigbee2mqtt
-git clone --depth 1 --branch 1.28.4 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
+sudo chown -R homeassistant: /opt/zigbee2mqtt
+sudo su homeassistant -c "git clone --depth 1 --branch 1.32.2 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt"
 
 cd /opt/zigbee2mqtt
-npm ci
+sudo su homeassistant -c  "npm ci"
 
 sudo chown -R homeassistant: /opt/zigbee2mqtt
 
@@ -80,7 +101,6 @@ WantedBy=multi-user.target
   " | sudo tee /etc/systemd/system/zigbee2mqtt.service
 
 sudo systemctl enable zigbee2mqtt.service
-sudo systemctl start zigbee2mqtt.service
 
 echo "Done with Zigbee2MQTT"
 echo "HA part 2 install"
@@ -88,14 +108,14 @@ echo "HA part 2 install"
 echo "[Unit]
 Description=Home Assistant
 After=network-online.target
+
 [Service]
 Type=simple
 Restart=on-failure
-
-User=%i
-WorkingDirectory=/srv/%i/
-ExecStart=/srv/homeassistant/bin/hass -c "/home/%i/.homeassistant"
-Environment="PATH=/srv/%i/bin"
+User=homeassistant
+WorkingDirectory=/srv/homeassistant/
+ExecStart=/srv/homeassistant/bin/hass -c "/srv/homeassistant/.homeassistant"
+Environment="PATH=/srv/homeassistant/bin"
 
 [Install]
 WantedBy=multi-user.target
@@ -114,18 +134,20 @@ sudo -u homeassistant -H -s bash -c "source bin/activate &&
 
 sudo -u homeassistant -H -s bash -c "cd /home/homeassistant/.homeassistant &&
                                      mkdir custom_components &&
+                                     mkdir media &&
+                                     mkdir .storage &&
                                      cd custom_components &&
-                                     wget https://github.com/airalab/homeassistant-robonomics-integration/archive/refs/tags/1.5.5.zip &&
-                                     unzip 1.5.5.zip &&
-                                     mv homeassistant-robonomics-integration-1.5.5/custom_components/robonomics . &&
-                                     rm -r homeassistant-robonomics-integration-1.5.5 &&
-                                     rm 1.5.5.zip "
+                                     wget https://github.com/airalab/homeassistant-robonomics-integration/archive/refs/tags/1.6.1.zip &&
+                                     unzip 1.6.1.zip &&
+                                     mv homeassistant-robonomics-integration-1.6.1/custom_components/robonomics . &&
+                                     rm -r homeassistant-robonomics-integration-1.6.1 &&
+                                     rm 1.6.1.zip "
 
 sudo systemctl restart home-assistant@homeassistant.service
 echo "DONE WITH ha part 2"
 
 echo "Clear garbage"
 cd /home/$USER
-rm complete_install.sh input.json yggdrasil-0.4.7-arm64.deb
+rm complete_install.sh
 
 echo "Garbage collected"
