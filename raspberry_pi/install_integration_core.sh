3aea2fa70a0b374cd923b833fbb496d770068ec0
nakata5321 <makar5321@gmail.com>
2023-09-22 12:06:02 +0300

    update core script

--- raspberry_pi/install_integration_core.sh
+++ raspberry_pi/install_integration_core.sh
@@ -7,10 +7,10 @@ rm install_ipfs_arc_dependent.sh
 sudo -u homeassistant -H -s bash -c "cd /home/homeassistant/.homeassistant &&
                                      mkdir custom_components &&
                                      cd custom_components &&
-                                     wget https://github.com/airalab/homeassistant-robonomics-integration/archive/refs/tags/1.5.3.zip &&
-                                     unzip 1.5.3.zip &&
-                                     mv homeassistant-robonomics-integration-1.5.3/custom_components/robonomics . &&
-                                     rm -r homeassistant-robonomics-integration-1.5.3 &&
-                                     rm 1.5.3.zip"
+                                     wget https://github.com/airalab/homeassistant-robonomics-integration/archive/refs/tags/1.6.1.zip &&
+                                     unzip 1.6.1.zip &&
+                                     mv homeassistant-robonomics-integration-1.6.1/custom_components/robonomics . &&
+                                     rm -r homeassistant-robonomics-integration-1.6.1 &&
+                                     rm 1.6.1.zip"
 
 echo "Integration downloaded!"
